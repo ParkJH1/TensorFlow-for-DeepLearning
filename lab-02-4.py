5cc1107062a7da93844c43a4de5c0c25d8347aa9
박정현 <pjh@bagjeonghyeon-ui-MacBook-Pro.local>
2019-12-26 21:51:52 +0900

    Update lab-02-4.py

--- lab-02-4.py
+++ lab-02-4.py
@@ -8,19 +8,19 @@ a = 5
 b = 2
 x_data = np.random.rand(N, 1)
 noise = np.random.normal(scale=0.1, size=(N, 1))
-y_data = np.reshape(a * x_data + b + noise, (-1))
+y_data = a * x_data + b + noise
 
 g = tf.Graph()
 with g.as_default() as graph:
     # 텐서보드 시각화를 위해서 네임스페이스 추가
     with tf.name_scope('placeholders'):
-        x = tf.placeholder(tf.float32)
-        y = tf.placeholder(tf.float32)
+        x = tf.placeholder(tf.float32, [N, 1])
+        y = tf.placeholder(tf.float32, [N, 1])
     with tf.name_scope('weights'):
-        W = tf.Variable(tf.random_normal([1]), name='weight')
+        W = tf.Variable(tf.random_normal([1, 1]), name='weight')
         b = tf.Variable(tf.zeros([1]), name='bias')
     with tf.name_scope('prediction'):
-        hypothesis = tf.squeeze(W * x + b)
+        hypothesis = tf.matmul(x, W) + b
     with tf.name_scope('loss'):
         loss = tf.reduce_mean(tf.square(y - hypothesis))
     with tf.name_scope('optimizer'):
