a1b483802b34dd6727f63b8e7fd97bdeedfe6806
박정현 <pjh@bagjeonghyeon-ui-MacBook-Pro.local>
2019-12-26 21:51:47 +0900

    Update lab-02-1.py

--- lab-02-1.py
+++ lab-02-1.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+# 선형 회귀 예시 데이터
 # 생성할 데이터 개수
 N = 100
 
@@ -9,7 +10,7 @@ a = 5
 b = 2
 x_data = np.random.rand(N, 1)
 noise = np.random.normal(scale=0.1, size=(N, 1))
-y_data = np.reshape(a * x_data + b + noise, (-1))
+y_data = a * x_data + b + noise
 
 plt.figure(0)
 plt.scatter(x_data, y_data)
