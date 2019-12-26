47b6518e9d6deca96e6033890bfe00b8269e5bf0
박정현 <pjh@bagjeonghyeon-ui-MacBook-Pro.local>
2019-12-26 21:51:50 +0900

    Update lab-02-3.py

--- lab-02-3.py
+++ lab-02-3.py
@@ -10,19 +10,15 @@ a = 5
 b = 2
 x_data = np.random.rand(N, 1)
 noise = np.random.normal(scale=0.1, size=(N, 1))
-y_data = np.reshape(a * x_data + b + noise, (-1))
-
-print(x_data)
-print(y_data)
-
+y_data = a * x_data + b + noise
 
 g = tf.Graph()
 with g.as_default() as graph:
-    x = tf.placeholder(tf.float32)
-    y = tf.placeholder(tf.float32)
-    W = tf.Variable(tf.random_normal([1]))
+    x = tf.placeholder(tf.float32, [N, 1])
+    y = tf.placeholder(tf.float32, [N, 1])
+    W = tf.Variable(tf.random_normal([1, 1]))
     b = tf.Variable(tf.random_normal([1]))
-    hypothesis = tf.squeeze(W * x + b)
+    hypothesis = tf.matmul(x, W) + b
     loss = tf.reduce_mean(tf.square(y - hypothesis))
     optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.01)
     train = optimizer.minimize(loss)
