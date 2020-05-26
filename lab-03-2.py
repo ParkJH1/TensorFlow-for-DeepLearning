efe162236136315488bcf98bd706cd880cb9fca9
박정현 <pjh9996@naver.com>
2020-05-26 11:36:30 +0900

    Update lab-03-2.py

--- lab-03-2.py
+++ lab-03-2.py
@@ -1,27 +1,30 @@
 import tensorflow as tf_new
 import numpy as np
 import matplotlib.pyplot as plt
-import mnist
 
 tf = tf_new.compat.v1
 tf.set_random_seed(777)
 
-x_data, y_data, _ = mnist.load(9)
+x_data = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
+y_data = np.array([[0], [1], [1], [0]])
 
 g = tf.Graph()
 with g.as_default() as graph:
-    x = tf.placeholder(tf.float32, [None, 784])
+    x = tf.placeholder(tf.float32, [None, 2])
     y = tf.placeholder(tf.float32, [None, 1])
-    W1 = tf.Variable(tf.random_normal([784, 100]))
-    b1 = tf.Variable(tf.zeros([100]))
+    W1 = tf.Variable(tf.random_normal([2, 2]))
+    b1 = tf.Variable(tf.zeros([2]))
     L1 = tf.sigmoid(tf.matmul(x, W1) + b1)
 
-    W2 = tf.Variable(tf.random_normal([100, 1]))
+    W2 = tf.Variable(tf.random_normal([2, 1]))
     b2 = tf.Variable(tf.zeros([1]))
-    hypothesis = tf.matmul(L1, W2) + b2
+    L2 = tf.matmul(L1, W2) + b2
 
-    loss = tf.reduce_mean(tf.square(hypothesis - y))
-    optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.01)
+    hypothesis = tf.sigmoid(L2)
+
+    entropy = tf.nn.sigmoid_cross_entropy_with_logits(logits=L2, labels=y)
+    loss = tf.reduce_sum(entropy)
+    optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.05)
     train = optimizer.minimize(loss)
 
     n_steps = 10000
@@ -38,3 +41,13 @@ with g.as_default() as graph:
         correct_prediction = tf.equal(pred_y, y_data)
         accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
         print("accuracy %s%%" % (sess.run(accuracy, feed_dict={x: x_data, y: y_data}) * 100))
+
+        plt.figure(0)
+        x_min, x_max = x_data[:, 0].min() - 1, x_data[:, 0].max() + 1
+        y_min, y_max = x_data[:, 1].min() - 1, x_data[:, 1].max() + 1
+        xx, yy = np.meshgrid(np.linspace(x_min, x_max, 100), np.linspace(y_min, y_max, 100))
+        Z = sess.run(tf.round(hypothesis), feed_dict={x: np.c_[xx.ravel(), yy.ravel()]})
+        Z = Z.reshape(xx.shape)
+        plt.contourf(xx, yy, Z, cmap=plt.cm.Spectral, alpha=0.8)
+        plt.scatter(x_data[:, 0], x_data[:, 1], c=y_data[:, 0], s=40, cmap=plt.cm.Spectral)
+        plt.show()
